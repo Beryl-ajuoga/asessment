b98777e09bb8173ed451ac554dcd2bf2164394c3
beryl-ajuoga <berrylajuoga@gmail.com>
2022-03-10 08:57:55 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var p="beryl"
+    var z =22
+
+
+    println("hi my name is $p , i am $z years old")
+
+
+    fun numbers() {
+        var name = "codehive"
+        var a = name[5].toString() + name[6] + name[7] + name[8]
+        println(a)
+    }
+
+
+        }
